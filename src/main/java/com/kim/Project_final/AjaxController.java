c67ae9a9ab3721e7710107b52ccdb0b8d3c6c417
wosad878 <wosad878@naver.com>
2019-01-09 20:49:42 +0900

    132

--- src/main/java/com/kim/Project_final/AjaxController.java
+++ src/main/java/com/kim/Project_final/AjaxController.java
@@ -1,29 +1,13 @@
 package com.kim.Project_final;
 
-import javax.inject.Inject;
-
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.kim.Project_final.Ajax.AjaxService;
 
 @RestController
 @RequestMapping("/ajax/**")
 public class AjaxController {
 
-	@Inject
-	private AjaxService ajaxService; 
-	
-	@RequestMapping(value="checkId",method=RequestMethod.POST)
-	public String checkId(String id) throws Exception {
-		String message = "사용 불가능 한 ID 입니다";
-		if(id != "") {
-			String dbId = ajaxService.checkId(id);
-			if(dbId == null) {
-				message = "사용가능 한 아이디 입니다";
-			}
-		}
-		return message;
+	public void checkId(String id) throws Exception {
+		
 	}
 }
--- src/main/webapp/WEB-INF/views/join/joinForm.jsp
+++ src/main/webapp/WEB-INF/views/join/joinForm.jsp
@@ -12,7 +12,17 @@
 <script type="text/javascript">
 	$(document).ready(function(){
 		
-		
+		$('.id').blur(function(){
+			var id = $(this).val();
+			$.ajax({
+				url:'/Project_final/ajax/checkId',
+				type:'post',
+				data:id,
+				success:function(data){
+					alert(data);
+				}
+			});
+		});
 		$('.adrbutton').click(function(){
 			goPopup();
 		});
@@ -28,17 +38,7 @@
 		});
 		
 	});
-	$('.id').blur(function(){
-		var id = $(this).val();
-		$.ajax({
-			url:'/Project_final/ajax/checkId',
-			type:'post',
-			data:id,
-			success:function(data){
-				alert(data);
-			}
-		});
-	});
+	
 	// opener관련 오류가 발생하는 경우 아래 주석을 해지하고, 사용자의 도메인정보를 입력합니다. ("팝업API 호출 소스"도 동일하게 적용시켜야 합니다.)
 	//document.domain = "abc.go.kr";
 

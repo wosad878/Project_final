0f6d8133b43ed0979e63413ce0bcbfc90c1c948d
wosad878 <wosad878@naver.com>
2019-01-11 14:16:22 +0900

    123

--- src/main/webapp/resources/js/header.js
+++ src/main/webapp/resources/js/header.js
@@ -11,12 +11,16 @@ $(function(){
 	$(window).bind('scroll', function() {
 		if ($(window).scrollTop() > 150) {
 			$('.lnb').css('position','fixed');
-			$('.lnb').css('top','41px');
-			$('.lnb').css('border-top','none');
+			$('.lnb').css('top','39px');
+			$('html').css('padding-top','50px');
+			$('.lnb').css('border-top','1px solid #e8e8e8');
+			$('.logoImg').css('display','none');
 		}
 		else if ($(window).scrollTop() < 150) {
 			$('.lnb').css('border-top','1px solid black');
 			$('.lnb').css('position','relative');
+			$('html').css('padding-top','0');
+			$('.logoImg').css('display','inline');
 			$('.lnb').css('top','0');
 		}	
 	});
